7b08f063b2491eb07887ce7036e8bacc6af777f6
brightstarlcg <32693154+brightstarlcg@users.noreply.github.com>
2019-10-30 20:19:43 +0800

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI:'mongodb+srv://text:lcg123456@houtai-sqmxn.azure.mongodb.net/test?retryWrites=true&w=majority',
+  mongoURI:'mongodb+srv://text:lch123457@houtai-sqmxn.azure.mongodb.net/test?retryWrites=true&w=majority',
   secretOrKey: 'secret'
 };
